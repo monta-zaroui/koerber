0dea19a8cb36e4c3be2c896887621a2062e5f932
monta-zaroui <montassar.zaroui@beneutron.com>
2023-07-18 23:06:26 +0200

    init

--- koerber-fe/src/App.vue
+++ koerber-fe/src/App.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
-import BaseInput from "./components/BaseInput.vue";
-import { ref } from "vue";
-
-const username = ref('');
 </script>
 
 <template>
-  <BaseInput v-model="username" placeholder="Enter your username" />
+  <button class="btn">Hello daisyui</button>
 </template>
