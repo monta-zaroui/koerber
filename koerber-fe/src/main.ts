30664b82124dce7eb8902aa880f66e231927a0e3
monta-zaroui <montassar.zaroui@beneutron.com>
2023-07-19 17:51:51 +0200

    setup font awesome component

--- koerber-fe/src/main.ts
+++ koerber-fe/src/main.ts
@@ -2,8 +2,20 @@ import { createApp } from 'vue';
 import './style.css';
 import { createPinia } from 'pinia';
 import App from './App.vue';
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import { faSort } from '@fortawesome/free-solid-svg-icons';
+
+/* add icons to the library */
+library.add(faSort);
 
 const pinia = createPinia();
 const app = createApp(App);
+app.component('FontAwesomeIcon', FontAwesomeIcon);
 app.use(pinia);
 app.mount('#app');
