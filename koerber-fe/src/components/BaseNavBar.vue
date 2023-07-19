29bf3f4264c48102db9f6101f21a4503b43221db
monta-zaroui <montassar.zaroui@beneutron.com>
2023-07-19 21:34:42 +0200

    fixed searchbar

--- koerber-fe/src/components/BaseNavBar.vue
+++ koerber-fe/src/components/BaseNavBar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="navbar container bg-accent top-4 rounded w-full shadow z-10">
-    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+  <div class="navbar container bg-accent top-4 fixed rounded w-full shadow z-10">
+    <a class="btn btn-ghost normal-case text-xl">Koerber</a>
   </div>
 </template>
